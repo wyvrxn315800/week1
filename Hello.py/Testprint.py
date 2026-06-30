44aebac98b0e7b7eb9cf847c00bf8619e88d6866
wyvrxn315800 <suphakeat.inkeaw007@gmail.com>
2026-06-30 15:02:16 +0700

    test

---
+++ Hello.py/Testprint.py
@@ -0,0 +1,10 @@
+print("\ta\na\ta\ta\n\ta")
+print('''\ta
+a\ta\a
+\ta''')
+
+print('100.00')
+print('%d' % 100)
+print('%d' % 100.58)
+print('%f' % -100.89)
+print('%.2f' % 100.89)
---
+++ Hello.py/main.py
@@ -0,0 +1,9 @@
+print("Hello, World!")
+print("Information and Network Engineering")
+print("Suphakeat You will be great")
+
+print("I'm Hungry")
+print("I want to eat some food")
+
+print("I'am good \"I will do my best\"")
+print("/\\")
