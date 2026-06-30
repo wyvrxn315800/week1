7fc7538a04d166fca03c84fd93c47bc29bdabea4
wyvrxn315800 <suphakeat.inkeaw007@gmail.com>
2026-06-30 15:32:33 +0700

    testmath

---
+++ Hello.py/testmath.py
@@ -0,0 +1,24 @@
+from math import pi
+print('%.2f' % pi)
+
+from math import pi
+print('%.4f' % pi)
+
+from math import pi
+print('%.60f' % pi)
+
+print('My age is', 25, 'I have', 3500.50, 'Bth')
+
+print('My age is %d I have %.2f Bth' % (25, 3500.50))
+
+print('My age is '+str(25)+' I have '+str(3500.50)+' Bth')
+
+print('5+4 =',5+4)
+print('5-4 = %.2f' % (5-4))
+print('5+4 = ' + str(5+4))
+
+print("I'm Suphakeat ", "I'll keep practicing!!")
+print("I'm Suphakeat " + "I'll keep practicing!!")
+
+print(1,   1,   2,   3,   5,   8,   13,   21,   34,   55)
+print('%d   %d   %d %d %d %d %d %d %d %d' % (1,1,2,3,5,8,13,21,34,55))
