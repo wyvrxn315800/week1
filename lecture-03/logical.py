89e124c9501a43141c8eed032a8e16f9dbbbe547
wyvrxn315800 <suphakeat.inkeaw007@gmail.com>
2026-07-14 15:40:59 +0700

    new

---
+++ lecture-03/logical.py
@@ -0,0 +1,9 @@
+x=10
+y=20
+z=30
+if x<y and y<z:
+    print ("x is less than y and y is less than z.")
+if x<y or y>z:
+    print("Either x is less than y or u is greaer than z.")
+if x<y or y>z:
+    print("is notgreater than y")
---
+++ lecture-03/logical1.py
@@ -0,0 +1,9 @@
+a=[1,2,3]
+b=a
+c=[1,2,3]
+d=[1,2,3]
+print(a is b)
+print(a is c)
+print(c is d)
+print(a==c)
+print(c==d)
